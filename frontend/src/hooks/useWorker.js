e4e339ef51d37d6efa2b75be1c5f1ce91e105583
itsarunkumarx <arunkumarpalani428@gmail.com>
2026-03-16 19:55:32 +0530

    chore: push useWorker.js separately

--- frontend/src/hooks/useWorker.js
+++ frontend/src/hooks/useWorker.js
@@ -2,9 +2,10 @@ import { useContext } from 'react';
 import { WorkerContext } from '../context/WorkerContextDefinition';
 
 export const useWorker = () => {
-    const context = useContext(WorkerContext);
-    if (!context) {
-        throw new Error('useWorker must be used within a WorkerProvider');
-    }
-    return context;
+  const context = useContext(WorkerContext);
+  if (!context) {
+    console.warn('useWorker: WorkerContext is not available. Make sure component is wrapped in WorkerProvider.');
+    return {};
+  }
+  return context;
 };
