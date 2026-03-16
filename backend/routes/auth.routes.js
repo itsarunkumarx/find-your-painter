3b42634813e476ba11d13f5f8cb5cf7f3038857a
itsarunkumarx <arunkumarpalani428@gmail.com>
2026-03-16 19:49:35 +0530

    chore: push auth.routes.js separately

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { registerUser, loginUser, googleAuthUser } from '../controllers/auth.controller.js';
+import { registerUser, loginUser, googleAuthUser, getSupportAdmin } from '../controllers/auth.controller.js';
+import { protect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/google', googleAuthUser);
+router.get('/support-admin', protect, getSupportAdmin);
 
 export default router;
 
